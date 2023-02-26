c3927f3f1397abf01ef705f566e55b6dc22ad1f2
Utkarsh3128 <ukumar21-cse@bvucoep.edu.in>
2023-02-27 00:19:48 +0530

    modified:   index.html 	modified:   index.js(added scroll animation)

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-wow = new Wow ({
-    boxClass: 'wow',
-    animateClass: 'animated',
-    offset: 0,
-    mobile: true,
-    live: true
-})
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
 
-wow.init();
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) {
+      return c / 2 * t * t + b;
+    }
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+var button1 = document.querySelector('.clickImg');
+
+button1.addEventListener('click', function () {
+  smoothScroll('#swipe', 1000);
+});
